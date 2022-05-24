56cd9b03ec2d2a94f142a5b80c092dd069dc4e75
Im-Maamba <nikilnimi@gmail.com>
2022-05-25 01:48:57 +0530

    fourth commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,16 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Links from "./screens/links/links";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-
+import Loginpage from "./screens/loginpage/loginpage";
+import Registerpage from "./screens/registerpage/registerpage";
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Landingpage />} exact />;
-      <Route path="/links" element={<Links />} exact />;
+      <Route path="/login" element={<Loginpage />} />;
+      <Route path="/register" element={<Registerpage />} />;
+      <Route path="/links" element={<Links />} />;
     </Routes>
     <Footer />
   </BrowserRouter>
---
+++ frontend/src/screens/registerpage/registerpage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Registerpage = () => {
+  return <div>registerpage</div>;
+};
+
+export default Registerpage;
